bd796edc7e7ada5b29c5b4168f398bf8ad573e4d
Bagindov <Bagindov@gmail.com>
2021-04-10 09:20:23 +0700

    add blog posts and sanity schema and remove category

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from "react"
+import image from "../dark_light_relaxing_hd-wallpaper-70213.jpg"
 
 export default function Home(){
-    return <h1>Home</h1>
+    return (
+        <main>
+            <img
+            src={image}
+            alt="dark_light"
+            className="absolute object-cover w-full h-full" 
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello</h1>
+            </section>
+        </main>
+    )
 }
